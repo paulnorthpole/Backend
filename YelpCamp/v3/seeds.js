d0e56e4adbbae106bb77d5dbb32641dde67bba97
Paul Charles <pcharles71@rocketmail.com>
2017-06-10 22:24:34 -0800

    Added comments model

--- YelpCamp/v3/seeds.js
+++ YelpCamp/v3/seeds.js
@@ -1,11 +1,22 @@
-var mongoose   = require("mongoose"),
-    Campground = require("./models/campground");
+var mongoose = require("mongoose"),
+    Comment  = require("./models/comment");
+  Campground = require("./models/campground");
 
 var data = [
   {
-    name: "Cloud's Rest",
-    image: "https://farm3.staticflickr.com/2238/1514148183_092606ba94.jpg",
+    name:        "Cloud's Rest",
+    image:       "https://farm3.staticflickr.com/2238/1514148183_092606ba94.jpg",
     description: "Ahoy, rough landlubber. go to isla de muerta."
+  },
+  {
+    name:        "Desert Mesa",
+    image:       "https://farm3.staticflickr.com/2789/4176189296_c51043f23b.jpg",
+    description: "Futile ionic cannons lead to the sonic shower."
+  },
+  {
+    name:        "Roster's Roost",
+    image:       "https://farm4.staticflickr.com/3464/3712326558_5a25585fe3.jpg",
+    description: "A shining form of grace is the reincarnation."
   }
 ];
 
@@ -16,9 +27,31 @@ function seedDB () {
       console.log(err);
     }
     console.log("removed campgrounds!");
+    // Add a few campgrounds
+    data.forEach(function(seed) {
+      Campground.create(seed, function(err, campground) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("added a campground");
+          //create a comment
+          Comment.create(
+            {
+              text:   "This place is great, but I wish there was internet",
+              author: "Homer"
+            }, function(err, comment) {
+              if (err) {
+                console.log(err);
+              } else {
+                campground.comments.push(comment);
+                campground.save();
+                console.log("Created new comment");
+              }
+            });
+        }
+      });
+    });
   });
-
-  // Add a few campgrounds
 
   // Add a few comments
 }
