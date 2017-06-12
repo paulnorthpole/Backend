2f8b4850185912a2f3295f87481d62397055b272
Paul Charles <PCharles71@RocketMail.com>
2017-06-11 21:16:28 -0800

    Got logout working

--- Authentication/Authdemo/app.js
+++ Authentication/Authdemo/app.js
@@ -31,7 +31,7 @@ app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/secret", function(req, res) {
+app.get("/secret", isLoggedIn, function(req, res) {
   res.render("secret");
 });
 
@@ -60,11 +60,24 @@ app.get("/login", function(req, res) {
 });
 
 //login logic
-app.post("/login",passport.authenticate("local", {
+app.post("/login", passport.authenticate("local", {
   successRedirect: "/secret",
   failureRedirect: "/login"
 }), function(req, res) {
 });
+
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+function isLoggedIn (req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
 
 app.listen(3000, function() {
   console.log("Server started........");
