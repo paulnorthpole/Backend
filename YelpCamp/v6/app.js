8a58bf86341fdb7ac7500dbb31ee3c943561453f
Paul Charles <PCharles71@RocketMail.com>
2017-07-01 14:31:17 -0800

    Add logout route

--- YelpCamp/v6/app.js
+++ YelpCamp/v6/app.js
@@ -170,6 +170,12 @@ app.post("/login", passport.authenticate("local",
   }), function(req, res) {
 });
 
+   // Logic Route
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/campgrounds");
+});
+
 app.listen(3000, function() {
   console.log("The YelpCamp Server Has Started!");
 });
