79a7c8a884e8fed7c664610e2d544374a8917be4
Paul Charles <PCharles71@RocketMail.com>
2017-11-28 16:50:25 -0900

    Fixed user must be logged in to create a campground, and added "created by" to the campground

--- YelpCamp/v9/models/campground.js
+++ YelpCamp/v9/models/campground.js
@@ -5,6 +5,13 @@ var campgroundSchema = new mongoose.Schema({
   name:        String,
   image:       String,
   description: String,
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+    username: String
+  },
   comments:    [
     {
       type: mongoose.Schema.Types.ObjectId,
@@ -12,4 +19,5 @@ var campgroundSchema = new mongoose.Schema({
     }
   ]
 });
+
 module.exports       = mongoose.model("Campground", campgroundSchema);
