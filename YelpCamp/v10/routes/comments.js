9a22aea87a3fa687dd02a2244fdf651349fddb50
Paul Charles <PCharles71@RocketMail.com>
2017-11-28 16:59:36 -0900

    INIT V10

---
+++ YelpCamp/v10/routes/comments.js
@@ -0,0 +1,55 @@
+var express    = require("express"),
+    router     = express.Router({mergeParams: true}),
+    Campground = require("../models/campground"),
+    Comment    = require("../models/comment");
+
+//  Comments New
+router.get("/new", isLoggedIn, function(req, res) {
+  // Find campground by id
+  Campground.findById(req.params.id, function(err, campground) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("comments/new", {campground: campground});
+    }
+  });
+});
+
+// Comments Create
+router.post("/", isLoggedIn, function(req, res) {
+  // lookup campground using ID
+  Campground.findById(req.params.id, function(err, campground) {
+    if (err) {
+      console.log(err);
+      res.redirect("/campgrounds");
+    } else {
+      // Create new comment
+      Comment.create(req.body.comment, function(err, comment) {
+        if (err) {
+          console.log(err);
+        } else {
+          // Add username and id to comment
+	        comment.author.id = req.user._id;
+	        comment.author.username = req.user.username;
+          // Save comment
+	        comment.save();
+          //Connect new comment to campground
+          campground.comments.push(comment);
+          campground.save();
+          // Redirect to campground show page
+          res.redirect("/campgrounds/" + campground._id);
+        }
+      });
+    }
+  });
+});
+
+// Middleware to see if user is logged in
+function isLoggedIn (req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+module.exports = router;
