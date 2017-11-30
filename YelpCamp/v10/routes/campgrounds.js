2fbd404b49dde32514e47e5df0494a4f17a8721f
Paul Charles <PCharles71@RocketMail.com>
2017-11-30 12:34:52 -0900

    Fixed "edit" and "delete" to only show and work for proper user.

--- YelpCamp/v10/routes/campgrounds.js
+++ YelpCamp/v10/routes/campgrounds.js
@@ -25,9 +25,9 @@ router.post("/", isLoggedIn, function(req, res) {
   var name          = req.body.name;
   var image         = req.body.image;
   var desc          = req.body.description;
-  var author = {
-  	id: req.user._id,
-	  username: req.user.username
+  var author        = {
+    id:       req.user._id,
+    username: req.user.username
   };
   var newCampground = {name: name, image: image, description: desc, author: author};
 
@@ -62,11 +62,65 @@ router.get("/:id", function(req, res) {
   });
 });
 
+// EDIT Campground Route
+router.get("/:id/edit", checkCampgroundOwnership, function(req, res) {
+  Campground.findById(req.params.id, function(err, foundCampground) {
+    res.render("campgrounds/edit", {campground: foundCampground});
+  });
+});
+
+// UPDATE CAMPGROUND ROUTE
+router.put("/:id", checkCampgroundOwnership, function(req, res) {
+  // Find and update the correct campground
+  Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground) {
+    if (err) {
+      res.redirect("/campgrounds");
+    } else {
+      // Redirect somewhere(show page)
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
+});
+
+// DESTROY CAMPGROUND ROUTE
+router.delete("/:id", checkCampgroundOwnership, function(req, res) {
+  Campground.findByIdAndRemove(req.params.id, function(err) {
+    if (err) {
+      res.redirect("/campgrounds");
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
+});
+
 // Middleware to see if user is logged in
 function isLoggedIn (req, res, next) {
-	if (req.isAuthenticated()) {
-		return next();
-	}
-	res.redirect("/login");
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
 }
+
+function checkCampgroundOwnership (req, res, next) {
+  // Is user logged in?
+  if (req.isAuthenticated()) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+      if (err) {
+        res.redirect("back");
+      } else {
+        // Does user own the campground
+        if (foundCampground.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+          console.log("You do not have permission to edit that");
+        }
+      }
+    });
+  } else {
+    console.log("You need to be logged in to do that");
+    res.redirect("back");
+  }
+}
+
 module.exports = router;
