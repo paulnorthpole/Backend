0332d1bb3dfe3bfd6338817220aba49198c56513
Paul Charles <PCharles71@RocketMail.com>
2017-11-30 14:21:45 -0900

    allow signed in user to edit or delete comments

--- YelpCamp/v10/routes/comments.js
+++ YelpCamp/v10/routes/comments.js
@@ -29,10 +29,10 @@ router.post("/", isLoggedIn, function(req, res) {
           console.log(err);
         } else {
           // Add username and id to comment
-	        comment.author.id = req.user._id;
-	        comment.author.username = req.user.username;
+          comment.author.id       = req.user._id;
+          comment.author.username = req.user.username;
           // Save comment
-	        comment.save();
+          comment.save();
           //Connect new comment to campground
           campground.comments.push(comment);
           campground.save();
@@ -44,12 +44,68 @@ router.post("/", isLoggedIn, function(req, res) {
   });
 });
 
+// Comments Edit Route
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
+  Comment.findById(req.params.comment_id, function(err, foundComment) {
+    if (err) {
+      res.redirect("back");
+    } else {
+      res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+    }
+  });
+});
+
+//Comment Update Route
+router.put("/:comment_id", checkCommentOwnership, function(req, res) {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
+    if (err) {
+        res.redirect("back")
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
+});
+
+// COMMENT DESTROY ROUTE
+router.delete("/:comment_id", checkCommentOwnership, function(req, res) {
+  //findByIdAndRemove
+  Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+    if (err) {
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
+});
+
 // Middleware to see if user is logged in
 function isLoggedIn (req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
   res.redirect("/login");
+}
+
+function checkCommentOwnership (req, res, next) {
+  // Is user logged in?
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, function(err, foundComment) {
+      if (err) {
+        res.redirect("back");
+      } else {
+        // Does user own the comment
+        if (foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+          console.log("You do not have permission to edit that");
+        }
+      }
+    });
+  } else {
+    console.log("You need to be logged in to do that");
+    res.redirect("back");
+  }
 }
 
 module.exports = router;
